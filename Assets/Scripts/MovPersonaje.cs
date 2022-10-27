ecc62722d142bdce0f16cb38c68509d51ed62968
juandaortega456 <juan_dav.ortega@uao.edu.co>
2022-10-27 12:01:50 -0500

    AnimacionesCorrerMejoradas
    Ya se termino la animacion correr y caminar

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MovPersonaje : MonoBehaviour
 {
-    public float velocidadMovimiento = 5.0f;
+    public float velocidadInicial = 5.0f;
     public float velocidadRotacion = 200.0f;
     private Animator player;
-    public float x, y;  
+    public float x, y, VelocidadMovimiento;  
 
     void Start()
     {
@@ -18,22 +18,48 @@ public class MovPersonaje : MonoBehaviour
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
+        VelocidadMovimiento = velocidadInicial;
+        if (Input.GetKey(KeyCode.LeftShift)){
+            VelocidadMovimiento = 10.0f;
+            if(y>0)
+            {
+                player.SetBool("PuedoCorrer", true);
+            }
+            else
+            {
+                player.SetBool("PuedoCorrer", false);
+            }
+        }
+        else
+        {
+            VelocidadMovimiento = velocidadInicial;
+           player.SetBool("PuedoCorrer", false);
+        }
 
+       
         transform.Rotate(0, x * Time.deltaTime * velocidadRotacion,0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
-
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(0, -y * Time.deltaTime * 2f, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(0, y * Time.deltaTime * 2f, 0);
-        }
-
+        transform.Translate(0, 0, y * Time.deltaTime * VelocidadMovimiento);
 
         player.SetFloat("VelX", x);
         player.SetFloat("VelY", y);
+
+        //CORRER 
+        if (Input.GetKey(KeyCode.LeftShift)){
+            VelocidadMovimiento = 10.0f;
+            if(y>0)
+            {
+                player.SetBool("PuedoCorrer", true);
+            }
+            else
+            {
+                player.SetBool("PuedoCorrer", false);
+            }
+
+        }
+        else
+        {
+            VelocidadMovimiento = velocidadInicial;
+           player.SetBool("PuedoCorrer", false);
+        }
     }
 }
